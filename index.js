9d678f20e65db7f380da8ff7bc51f95a77e1f7aa
Ryan Mark <ryan@mrk.cc>
2013-04-01 09:36:29 -0500

    messing around, requires changes in voxel-player & voxel-control for flight mode

--- index.js
+++ index.js
@@ -1,45 +1,57 @@
 var createGame = require('voxel-engine')
 var highlight = require('voxel-highlight')
 var player = require('voxel-player')
-var texturePath = require('painterly-textures')(__dirname)
 var voxel = require('voxel')
 var extend = require('extend')
 
+var createTerrain = require('voxel-perlin-terrain');
+
+var playerPhysics = require('player-physics')
+
+
 module.exports = function(opts, setup) {
-  setup = setup || defaultSetup
-  var defaults = {
-    generate: voxel.generator['Valley'],
+  // setup the game and add some trees
+  var game = createGame({
+    //generateVoxelChunk: createTerrain(2,32),
+    generate: function(x, y, z) {
+      if(y > 20) return 0
+      else if(y> 14) return 1
+      else if(y> 3) return 3
+      else return 4
+    },
     chunkDistance: 2,
     materials: [
-      ['grass', 'dirt', 'grass_dirt'],
-      'obsidian',
-      'brick',
-      'grass',
-      'plank'
+      ['grass_top', 'dirt', 'grass_side'],
+      'gravel',
+      'stone',
+      'bedrock',
+      'wood',
+      'brick'
     ],
-    texturePath: texturePath,
+    texturePath: '/textures/crayoncraft/textures/blocks/',
     worldOrigin: [0, 0, 0],
-    controls: { discreteFire: true }
-  }
-  opts = extend({}, defaults, opts || {})
-
-  // setup the game and add some trees
-  var game = createGame(opts)
+    controls: { discreteFire: true },
+    lightsDisabled: false,
+    fogDisabled: false,
+    generateChunks: true,
+    mesher: voxel.meshers.greedy,
+    playerHeight: 1.62
+  })
   var createPlayer = player(game)
 
   window.game = game // for debugging
-  var container = opts.container || document.body
+  var container = document.body
 
   game.appendTo(container)
 
   // create the player from a minecraft skin file and tell the
   // game to use it as the main player
-  var avatar = createPlayer(opts.playerSkin || 'player.png')
+  var avatar = createPlayer('player.png')
   avatar.possess()
-  avatar.yaw.position.set(2, 14, 4)
+  avatar.yaw.position.set(2, 50, 4)
 
-  setup(game, avatar)
-  
+  defaultSetup(game, avatar)
+  //game.controlling.removeForce(game.gravity)  //FLY!
   return game
 
 }
@@ -53,9 +65,15 @@ function defaultSetup(game, avatar) {
   hl.on('highlight-adjacent', function (voxelPos) { blockPosPlace = voxelPos })
   hl.on('remove-adjacent', function (voxelPos) { blockPosPlace = null })
 
-  // toggle between first and third person modes
   window.addEventListener('keydown', function (ev) {
-    if (ev.keyCode === 'R'.charCodeAt(0)) substack.toggle()
+    // toggle between first and third person modes
+    if (ev.keyCode === 'R'.charCodeAt(0)) avatar.toggle()
+
+    // toggle fly mode
+    // TODO: make it not suck
+    else if (ev.keyCode === 'G'.charCodeAt(0)) {
+      avatar.toggleFlight()
+    }
   })
 
   // block interaction stuff, uses highlight data
